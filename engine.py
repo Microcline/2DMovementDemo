bdc0eec18339381af3cbb99677046c82b47225db
Microcline <Microcline@users.noreply.github.com>
2023-01-25 14:51:58 -0600

    changes (#5)

--- engine.py
+++ engine.py
@@ -1,11 +1,3 @@
-"""
-Author: 
-Date:   
-"""
-"""
-Author: 
-Date:   
-"""
 import tcod as libtcod
 from input_handlers import handle_keys
 from entity import Entity
@@ -16,24 +8,30 @@ def main():
     # Screen vars
     screen_width = 80
     screen_height = 50
+    # Map vars
     map_width = 80 
     map_height = 45 #comment
+    # Room vars
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
-    # Console vars
-    con = libtcod.console_new(screen_width, screen_height)
-    game_map = GameMap(map_width, map_height)
-    game_map.make_map()
+    # Entity instantiation and dictionary tracking
+    player  = Entity(21, 16, '@', libtcod.white)
+    npc     = Entity(10, 10, '@', libtcod.yellow)
+    entities = [player, npc]
 
+    # Expandable color set
     colors = {'dark_wall': libtcod.Color(0, 0, 100),
               'dark_ground': libtcod.Color(50, 50, 150)
     }
 
-    # Entity instantiation and dictionary tracking
-    player  = Entity(1, 1, '@', libtcod.white)
-    npc     = Entity(10, 10, '@', libtcod.yellow)
-    entities = [player, npc]
+    # Console vars
+    con = libtcod.console_new(screen_width, screen_height)
+    game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
-    # What does this do?
+    # Font and window header configuration
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
     libtcod.console_init_root(screen_width, screen_height, 'Little Red Wagon Autofighter', False)
 
@@ -41,21 +39,18 @@ def main():
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
-    # Game loop
+    # Game loop ================================================================================
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
-
         render_all(con, entities, game_map, screen_width, screen_height, colors)
         libtcod.console_flush()
         #libtcod.console_put_char(con, player.x, player.y, ' ', libtcod.BKGND_NONE)
         clear_all(con, entities)
-        
         # Gather keypress
         action = handle_keys(key)
         move = action.get('move')
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')  
-
         if move:
             deltaX, deltaY = move
             if not game_map.is_blocked(player.x + deltaX, player.y + deltaY):
@@ -67,9 +62,7 @@ def main():
         # Exit the console
         if key.vk == libtcod.KEY_ESCAPE:
             return True
-    # End game loop
-
-
+    # End game loop ==============================================================================
 
 if __name__ == '__main__':
-    main()  # comment for change
+    main()
